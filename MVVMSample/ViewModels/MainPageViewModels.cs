d851bd15e48772006efc02518abed3269181e75b
mmmmmiya <fumina.miya@gmail.com>
2016-10-14 23:37:45 +0900

    first commit

---
+++ MVVMSample/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace MVVMSample.ViewModels
+{
+	public class MainPageViewModels : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		string text,aText;
+		
+		public MainPageViewModels()
+		{
+
+		}
+
+		public string Text {
+			get
+			{
+				return text;
+			}
+			set 
+			{
+				if (text != value)
+				{
+					text = value;
+					OnPropertyChanged("Text");
+					SetText();
+					OnPropertyChanged("AText");
+				}
+				
+			}
+		}
+
+		public string AText
+		{
+			get
+			{
+				return aText;
+			}
+			set { }
+		}
+
+		void SetText()
+		{
+			aText = text;
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this,
+					new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
+
